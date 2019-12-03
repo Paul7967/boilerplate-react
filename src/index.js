40ecb5a1f4120b1f7153a3c49e67674ebac1aafd
Paul7967 <postatochnikov@yandex.ru>
2019-12-04 01:19:35 +0500

    Third commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.sass';
+
+const App = () => <p>Hello234</p>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
